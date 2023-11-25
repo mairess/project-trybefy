f524eecf3806421b856380477fe791f790587b33
mairess <trabalho.maires@gmail.com>
2023-11-25 07:19:00 -0300

    query: create and populate tables

---
+++ src/02_challenge.sql
@@ -0,0 +1,34 @@
+USE Trybefy;
+
+CREATE TABLE plans (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(50) NOT NULL,
+    price FLOAT(5,2) NOT NULL
+);
+
+INSERT INTO plans(name, price)
+VALUES('Anual', 39.90),
+      ('Mensal', 5.90),
+      ('Trimestral', 19.90);
+
+CREATE TABLE users (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    full_name VARCHAR(50) NOT NULL,
+    email VARCHAR(50) NOT NULL,
+    birthday DATE NOT NULL,
+    active BOOLEAN NOT NULL DEFAULT 1,
+    plan_id INT NOT NULL,
+    FOREIGN KEY (plan_id) REFERENCES plans(id)
+);
+
+INSERT INTO users(full_name, email, birthday, active, plan_id)
+VALUES('Pedro', 'pedro@trybefy.com', '1990-01-01', 1, 1),
+      ('Camila', 'camila@trybefy.com', '1988-12-01', 1, 1),
+      ('Guilherme', 'guilherme@trybefy.com', '1988-12-01', 1, 2),
+      ('Andressa', 'andressa@trybefy.com', '1984-07-20', 0, 3),
+      ('Luís', 'luis@trybefy.com', '2000-01-01', 1, 2),
+      ('Cássia', 'cassia@trybefy.com', '1995-12-01', 1, 1),
+      ('Simone', 'simone@trybefy.com', '1988-12-01', 1, 1),
+      ('Rogério', 'rogerio@trybefy.com', '1979-12-01', 1, 3),
+      ('Júlio', 'julio@trybefy.com', '1994-06-20', 0, 2),
+      ('Melissa', 'melissa@trybefy.com', '1997-04-11', 1, 2);
